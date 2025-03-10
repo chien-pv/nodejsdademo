b9b7e623628e0fecf116b8821ebe2a322455085a
chien-pv <phamvanbk11@gmail.com>
2025-03-10 14:14:33 +0700

    demo product

--- demoST23A/routes/root.js
+++ demoST23A/routes/root.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const HomeController = require("../controllers/home_controller");
+const ProductController = require("../controllers/product_controller");
 
-router.get("/:id", HomeController.index);
+router.get("/", HomeController.index);
+router.get("/products", ProductController.index);
 router.post("/:id", HomeController.index);
 
 module.exports = router;
